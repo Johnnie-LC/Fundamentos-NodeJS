c0c90f22b061971a0bc16c44d96ff543ca6ad5d6
Johnnie Lopez <johesloc@gmail.com>
2021-07-13 21:01:22 -0500

    add entorno file where use varables enviroments in node

---
+++ conceptos/entorno.js
@@ -0,0 +1,9 @@
+// para acceder a las variables de entorno debemos usar process.env y la variable deseada en mayusculas
+// y enterminal podemos asignar un valor a la variable, por ejemplo: NOMBRE='Johnnie'
+// Para dar un valor por defecto a nuestra variabla podemos usar ||
+// Como buenas practicas, las variables de entorno se recomienda escribirlas en mayusculas
+// en el caso que sea dos palabras se separan con un guion bajo
+let nombre = process.env.NOMBRE || 'Sin Nombre'
+let web = process.env.MI_WEB || 'no tengo web'
+console.log('Hola ' + nombre)
+console.log('Mi Web es ' + web)
