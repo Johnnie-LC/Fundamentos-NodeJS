f3b66010df1c97915585b51e922655e9de19e356
Johnnie Lopez <johesloc@gmail.com>
2021-07-14 14:10:39 -0500

    use Global in Node js

---
+++ modulos/globales.js
@@ -0,0 +1,27 @@
+// let i = 0
+
+// let intervalo = setInterval(() => {
+//   console.log('Hola')
+//   if (i === 3) {
+//     clearInterval(intervalo)
+//   }
+//   i++
+// }, 1000)
+
+// setImmediate(() => {
+//   console.log('Hola inmediato')
+// })
+
+// // para los procesos
+// console.log(process)
+
+// // direccion en donde estamos
+// console.log(__dirname)
+
+// // Nombre del fichero
+// console.log(__filename)
+
+// // recomendacion no usar variables globales - puedes ser el foco de muchos problemas
+// // variables Globales
+// globalThis.miVariable = 'elValor'
+// console.log(miVariable)
