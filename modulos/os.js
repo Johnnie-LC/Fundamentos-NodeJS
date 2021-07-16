307d3e4080ec5d30e202e1f0a477d53061c6bd83
Johnnie Lopez <johesloc@gmail.com>
2021-07-15 19:29:44 -0500

    use of the os module in Node JS

---
+++ modulos/os.js
@@ -0,0 +1,36 @@
+// to obtain all about of Operet system
+const os = require('os')
+
+// to obtain architecture x64
+// console.log(os.arch())
+
+// platform
+// console.log(os.platform())
+
+// cpus
+// console.log(os.cpus())
+// console.log(os.cpus().length)
+
+// constantes del sistema
+// console.log(os.constants)
+
+// ram memory
+// const SIZE = 1024
+// const kb = (bytes) => bytes / SIZE
+// const mb = (bytes) => kb(bytes) / SIZE
+// const gb = (bytes) => mb(bytes) / SIZE
+// console.log(os.freemem())
+// console.log(kb(os.freemem()))
+// console.log(mb(os.freemem()))
+// console.log('memoria libre: ', gb(os.freemem()))
+
+// console.log('memoria total: ', gb(os.totalmem()))
+
+// directorio raiz del usuario
+// console.log(os.homedir())
+// directorio de archivos temporales
+// console.log(os.tmpdir())
+
+// para servidores hostname
+// console.log(os.hostname())
+// console.log(os.networkInterfaces())
